b1255b840147a78fbbcd48b9c42676471e6d63ca
Joao <joaommattedi@gmail.com>
2023-08-19 19:38:49 -0300

    feat: create meal route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,34 @@
 import { FastifyInstance } from 'fastify'
+import { boolean, string, z } from 'zod'
+import { knex } from '../database'
 
 export async function mealsRoute(app: FastifyInstance) {
-  app.get('/', async (_req, res) => {
-    return res.status(200).send('Route working')
+  app.post('/', async (req, res) => {
+    const mealBodySchema = z.object({
+      name: string(),
+      description: string().optional(),
+      mealTime: string(),
+      allowedEat: boolean(),
+    })
+
+    const { name, description, mealTime, allowedEat } = mealBodySchema.parse(
+      req.body,
+    )
+
+    const { sessionId } = req.cookies
+
+    if (!sessionId) {
+      return res.status(401).send('User not logged')
+    }
+
+    const meal = await knex('meals').insert({
+      allowed_eat: allowedEat,
+      description,
+      meal_time: mealTime,
+      name,
+      user_uuid: sessionId,
+    })
+
+    return res.status(200).send(meal)
   })
 }
